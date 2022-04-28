5136523e1a3c866925e836746f28780c601e431e
Andres Arevalo <andresfelipear@gmail.com>
2022-04-27 20:54:56 -0700

    updated description and image world holidays

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -23,8 +23,8 @@ function Portfolio() {
       <Projects>
         <ProjectImg src='/images/projects/page3.jpg'/>
         <ProjectContent>
-          <PContentTitle>Tu Campamento</PContentTitle>
-          <PContentDes>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae cum animi repellat ipsum, facilis, asperiores iste eaque cumque neque enim placeat ipsam sunt quibusdam? Ratione deleniti nulla molestias ad fugit.</PContentDes>
+          <PContentTitle>World Holidays</PContentTitle>
+          <PContentDes>Project that allow the user to consult the holidays per Country and State in any place of the World. Currently because I'm using a free api key, I can only display the holidays in the previous year (2021).</PContentDes>
           <VisitButton><ButtonLink href={"https://world-holidays1305.netlify.app/"}>Visit Site</ButtonLink></VisitButton>
         </ProjectContent>
       </Projects>
