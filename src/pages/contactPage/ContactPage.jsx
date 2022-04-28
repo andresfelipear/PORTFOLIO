8052ddb23e1dc51305893983d8fb3398004b0cb3
Andres Arevalo <andresfelipear@gmail.com>
2022-04-27 23:05:58 -0700

    process contact form using emailjs

--- src/pages/contactPage/ContactPage.jsx
+++ src/pages/contactPage/ContactPage.jsx
@@ -4,8 +4,9 @@ import { Section, Form, Button, Box, Heading } from "react-bulma-components"
 import { useNavigate } from 'react-router-dom'
 import Modal from '../../components/notification/Modal';
 import "./ContactPage.css"
-function ContactPage() {
+import emailjs from "@emailjs/browser"
 
+function ContactPage() {
     const [name, setname] = useState("")
     const [phone, setPhone] = useState("")
     const [email, setEmail] = useState("")
@@ -39,9 +40,17 @@ function ContactPage() {
 
     //end modal
 
-    const submit = () => {
-        const body = { name, phone, email, message }
-        console.log(body)
+    const submit = (e) => {
+        e.preventDefault();
+        emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, e.target, process.env.REACT_APP_PUBLIC_KEY).
+            then((res) => {
+                openModal("Confirmation Message", "The Message was send! Thanks for contact us")
+                setStatus("success")
+            }).catch((err) => {
+                console.log(err)
+                openModal("Error Contact", "Something went wrong. Try Again!");
+                setStatus("")
+            })
     }
 
     useEffect(() => {
@@ -54,10 +63,11 @@ function ContactPage() {
 
     return (
         <Section mt={3} >
-            <Box style={{ width: 510, margin: 'auto', padding: "70px 50px", backgroundColor: "white",border:'2px solid var(--maincolor)' }}>
-                    <Heading textAlign="center" style={{color:'rgba(85, 0, 197, 0.72)'}}>Who are you?</Heading>
+            <Box style={{ width: 510, margin: 'auto', padding: "70px 50px", backgroundColor: "white", border: '2px solid var(--maincolor)' }}>
+                <form onSubmit={submit}>
+                    <Heading textAlign="center" style={{ color: 'rgba(85, 0, 197, 0.72)' }}>Who are you?</Heading>
                     <Form.Field>
-                        <Form.Label>Name <span style={{color:'var(--maincolor)', fontWeight:'bolder', fontSize:'15px'}}>&#42;</span></Form.Label>
+                        <Form.Label>Name <span style={{ color: 'var(--maincolor)', fontWeight: 'bolder', fontSize: '15px' }}>&#42;</span></Form.Label>
                         <Form.Control>
                             <Form.Input value={name} type="text" name="name" onChange={(e) => { setname(e.target.value) }} />
                         </Form.Control>
@@ -71,29 +81,30 @@ function ContactPage() {
                     </Form.Field>
 
                     <Form.Field>
-                        <Form.Label>Email <span style={{color:'var(--maincolor)', fontWeight:'bolder', fontSize:'15px'}}>&#42;</span></Form.Label>
+                        <Form.Label>Email <span style={{ color: 'var(--maincolor)', fontWeight: 'bolder', fontSize: '15px' }}>&#42;</span></Form.Label>
                         <Form.Control>
                             <Form.Input value={email} type="email" name="email" onChange={(e) => { setEmail(e.target.value) }} />
                         </Form.Control>
                     </Form.Field>
 
                     <Form.Field>
-                        <Form.Label>Message <span style={{color:'var(--maincolor)', fontWeight:'bolder', fontSize:'15px'}}>&#42;</span></Form.Label>
+                        <Form.Label>Message <span style={{ color: 'var(--maincolor)', fontWeight: 'bolder', fontSize: '15px' }}>&#42;</span></Form.Label>
                         <Form.Control>
-                            <Form.Textarea value={message} name="message" onChange={(e) => { setMessage(e.target.value) }} />
+                            <Form.Textarea value={message} name="message" onChange={(e) => {setMessage(e.target.value) }} />
                         </Form.Control>
                     </Form.Field>
 
                     <Button.Group align="center" mt={5}>
                         <Button
-                            style={disabled?({borderColor:'var(--maincolor)', color:`var(--maincolor)`}):({backgroundColor:'var(--maincolor)', color:`white`})}
+                            style={disabled ? ({ borderColor: 'var(--maincolor)', color: `var(--maincolor)` }) : ({ backgroundColor: 'var(--maincolor)', color: `white` })}
                             color={disabled ? ("") : ("link")}
-                            onClick={submit}
                             disabled={disabled}
-                            submit={false}
+                            type='submit'
                         >Send
                         </Button>
                     </Button.Group>
+                </form>
+
             </Box>
 
             <Modal notiTitle={notiTitle} notiBody={notiBody} handleClose={closeModal} />
