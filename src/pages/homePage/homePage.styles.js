c5964049e9bab8c3897fe2061d92493c609976ba
Andres Arevalo <andresfelipear@gmail.com>
2022-05-03 05:14:31 -0700

    styling, responsiveness

--- src/pages/homePage/homePage.styles.js
+++ src/pages/homePage/homePage.styles.js
@@ -9,33 +9,99 @@ export const MainContainer = styled.div`
         to {opacity: 1; transform: translateY(0);}
     }
 
-    @media (max-width:767px){
+    /* The typing effect */
+    @keyframes typing {
+    from { width: 0; color:#4A4A4A;}
+    to { width: 35vw; color:#4A4A4A; }
+    }
+
+    /* The typing effect */
+    @keyframes typing2 {
+    from { width: 0; color:var(--maincolor) }
+    to { width: 50vw; color:var(--maincolor) }
+    }
+
+    /* The typewriter cursor effect */
+    @keyframes blink-caret {
+    from, to { border-color: var(--maincolor) }
+    50% { border-color: transparent }
+    }
+
+    @media (min-width:769px) and (max-width:1440px){
+        font-size: 6.5rem;  
+    }
+
+    @media (max-width:768px){
         font-size: 5rem;  
     }
 
-    @media (max-width:320px){
-        font-size: 3.75rem;  
+    @media (max-width:425px){
+        font-size: 3.3rem;  
+        padding: 20px 40px;
+        text-align:center;
+
+        /* The typing effect */
+        @keyframes typing {
+        from { width: 0; color:#4A4A4A;}
+        to { width: 80vw; color:#4A4A4A; }
+        }
+
+        /* The typing effect */
+        @keyframes typing2 {
+        from { width: 0; color:var(--maincolor) }
+        to { width: 80vw; color:var(--maincolor) }
+        }
     }
 
     
 `;
 
 export const FirstLine = styled.div`
-    animation:main-text 0.6s cubic-bezier(0.2, 0.6, 0.2, 1);
-	animation-fill-mode:backwards;
+    color:#4A4A4A;
+    width: fit-content;
+    overflow: hidden; 
+    border-right: .05em solid transparent;
+    white-space: nowrap; 
+    letter-spacing: .15em; 
+    animation: 
+    typing 3.5s steps(40, end),
+    blink-caret .75s step-end 5;
+
+    @media (max-width:425px){
+        margin: 0 auto;
+    }
+
 `
 
 export const SecondLine = styled.div`
-    animation:main-text 0.6s cubic-bezier(0.2, 0.6, 0.2, 1);
-    animation-delay: 0.4s;
-    animation-fill-mode:backwards;
+    color:transparent;
+    width: fit-content;
+    overflow: hidden; 
+    border-right: .05em solid transparent; 
+    white-space: nowrap; 
+    letter-spacing: .15em; 
+    animation: 
+    typing 3.5s steps(40, end) 3.5s forwards,
+    blink-caret .75s step-end 3.5s 5;
+    @media (max-width:425px){
+        margin: 0 auto; 
+    }
+
 `
 
 export const ThirdLine = styled.div`
-    color:var(--maincolor);
-    animation:main-text 0.6s cubic-bezier(0.2, 0.6, 0.2, 1);
-    animation-delay: 1s;
-    animation-fill-mode:backwards;
+    color:transparent;
+    width: fit-content;
+    overflow: hidden; 
+    border-right: .05em solid transparent;
+    white-space: nowrap; 
+    letter-spacing: .15em;
+    animation: 
+    typing2 4.5s steps(40, end) 7s forwards,
+    blink-caret .75s step-end 7s 6;
+    @media (max-width:425px){
+        margin: 0 auto;
+    }
 `
 
 export const MainPhoto = styled.div`
@@ -46,7 +112,7 @@ export const MainPhoto = styled.div`
     top:8vh;
     right: 0; 
     bottom: 0; 
-    left: 48vw;
+    left: 56vw;
     background-image: url('/images/photo.png');
     background-repeat: no-repeat; 
     background-size: auto 90%;
@@ -59,23 +125,34 @@ export const MainPhoto = styled.div`
         0% {opacity: 0;transform: scale(1.2);}
         100% {opacity: 1;transform: scale(1);}	
     }
-    @media (min-width:1440px){
-        left:50vw;    
+
+    @media (min-width:769px) and (max-width:1440px){
+        height:600px; 
+        left: 58vw;
     }
 
-    @media (max-width:767px){
-        top:7.5vh;
+    @media (max-width:768px){
+        height:500px;
+        top:8.5vh;
         filter:blur(0.5px) grayscale(70%);
-        left:10vw;
-        right:10vw;    
-        background-size: 16rem;
+        left:60vw;   
     }
 
-    @media (max-width:320px){
-        filter:blur(0.5px) grayscale(70%);
-        left:4vw;
-        right:4vw;    
-        background-size: 16rem;
+    @media (max-width:425px){
+        top:8.5vh;
+        height:330px;
+        filter:blur(0.8px) grayscale(70%);
+        left:25vw;
+        right:25vw;    
+    }
+
+
+    @media (max-width:375px){
+        top:11.5vh;
+        height:330px;
+        filter:blur(0.8px) grayscale(70%);
+        left:20vw;
+        right:20vw;    
     }
 
 `
